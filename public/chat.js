4235c014e00ea1a39ee449da8fe7a7f52668dd28
EdgarAguilarGarcia <72715358+EdgarAguilarGarcia@users.noreply.github.com>
2023-02-21 19:59:50 -0600

    Add files via upload

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 //Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://wasapo.herokuapp.com/');
 
 //Query DOM
 var mensaje = document.getElementById('mensaje');
